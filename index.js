422a4ac30c0366177d01a363cf868534b72fb8b1
anastasia Ukhtinskaya <anastassia94@gmail.com>
2020-07-13 00:31:37 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,3 +2,7 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
   object[key] = value;
   return object;
 }
+
+function updateObjectWithKeyAndValue(object, key, value) {
+  return object.assign(key: "value");
+}
